21b47d77867783dbe6e0c2d6d14bbd2134eb7d6c
Sead Feng <seadfeng@gmail.com>
2024-08-15 17:18:41 +0800

    update headers

--- src/app/api/redirectcheck/route.ts
+++ src/app/api/redirectcheck/route.ts
@@ -6,19 +6,21 @@ export const runtime = 'edge';
 
 const BodySchema = z.object({
   url: z.string().url()
-})
+});
 
-const fetchUrl = async ({ url, headers }: { url: string, headers: HeadersInit }): Promise<ResponseInfo> => {
+const fetchUrl = async ({ url, headers }: { url: string, headers?: Headers }): Promise<ResponseInfo> => {
   const startTime = Date.now();
   const newUrl = new URL(url);
+
   const response = await fetch(url, {
     method: "GET",
     redirect: "manual",
     headers
   });
-  const duration = ((Date.now() - startTime) / 1000).toFixed(3);
 
+  const duration = ((Date.now() - startTime) / 1000).toFixed(3);
   const location = response.headers.get('location');
+
   return {
     url: url,
     host: newUrl.host,
@@ -26,14 +28,14 @@ const fetchUrl = async ({ url, headers }: { url: string, headers: HeadersInit })
     statusText: response.statusText,
     duration: `${duration} s`,
     location
-  }
-}
-
+  };
+};
 
 export async function POST(request: NextRequest & { cf?: Record<string, any> }) {
-  const jsonData = await request.json();
   let url: string;
+
   try {
+    const jsonData = await request.json();
     url = BodySchema.parse(jsonData).url;
   } catch (error: any) {
     return new Response(JSON.stringify({ error: { message: error.message } }, null, 2), {
@@ -41,27 +43,32 @@ export async function POST(request: NextRequest & { cf?: Record<string, any> }) 
       headers: {
         'Content-Type': 'application/json',
       }
-    })
+    });
   }
+
   let process = true;
   let data: ResponseInfo[] = [];
 
-  request.headers.delete('Content-Length');
+  const headers = new Headers(request.headers);
+  headers.delete('Content-Length');
 
   const maxTry = 10;
   let i = 0;
+
   while (process && i < maxTry) {
     try {
-      const responseInfo = await fetchUrl({ url, headers: request.headers });
+      const responseInfo = await fetchUrl({ url, headers });
       data.push(responseInfo);
+
       if (responseInfo.location) {
         url = responseInfo.location;
       } else {
         process = false;
       }
+
       i++;
     } catch (error) {
-      console.error(error)
+      console.error(error);
       data.push({
         url,
         host: new URL(url).host,
@@ -79,7 +86,5 @@ export async function POST(request: NextRequest & { cf?: Record<string, any> }) 
     headers: {
       'Content-Type': 'application/json'
     }
-  })
-
-
+  });
 }
