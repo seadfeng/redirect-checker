8ec4c458b475afb6d067565ffa1f0e0e2e7aee32
Sead Feng <seadfeng@gmail.com>
2024-08-20 22:45:17 +0800

    update target

--- src/components/frontend/shared/footer.tsx
+++ src/components/frontend/shared/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <footer className="container py-4 text-center text-sm text-muted-foreground mt-10">
       <div className="my-3 flex items-center justify-center">
         <div className="mr-3">Our Products: </div>
-        <a href="https://www.faviconextractor.com/" target="_blank" className="text-primary flex items-center">
+        <a href="https://www.faviconextractor.com/" className="text-primary flex items-center">
           <img alt="Favicon Extractor" src="https://www.faviconextractor.com/favicon/www.faviconextractor.com" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
           <span>Favicon Extractor</span>
         </a>
