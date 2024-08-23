3f1963619b4e494816aa25e530c92848419dcc28
Sead Feng <seadfeng@gmail.com>
2024-08-23 17:46:14 +0800

    add alternates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,9 @@
-import { appConfig } from "@/config";
+import { appConfig, LocaleType } from "@/config";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-export const canonicalLink = (domain: string, pathname: string) => {
-  const isProduction = process.env.NODE_ENV === 'production';
-  return `${isProduction ? "https" : "http"}://${domain}${pathname}`;
 }
 
 export const getOrigin = ({ headers }: { headers: Headers }) => {
@@ -15,4 +11,31 @@ export const getOrigin = ({ headers }: { headers: Headers }) => {
 
   const protocol = ['localhost', '127.0.0.1'].includes(host.split(":")[0]) ? 'http' : 'https';
   return `${protocol}://${host}`;
+}
+
+export const getCanonical = ({ headers }: { headers: Headers }) => {
+  const origin = getOrigin({ headers });
+  const url = new URL(headers.get("x-request-url")!);
+  return `${origin}${url.pathname}`;
+}
+
+export const createAlternates = ({ headers, canonical }: { headers: Headers; canonical: string; }) => {
+  let languages = {} as Record<LocaleType, string>;
+  const linkStr = headers.get("Link")!;
+  const links = linkStr.split(',');
+  links.forEach(alternateStr => {
+    const match = alternateStr.match(/<(.*)>;.*hreflang="(.*)"/);
+    if (match && match[1]) {
+      if (match[2] !== "x-default") {
+        languages[match[2] as LocaleType] = match[1];
+      } else {
+        languages[appConfig.i18n.defaultLocale] = match[1];
+      }
+    }
+  })
+
+  return {
+    canonical,
+    languages
+  }
 }
