faca6f0ec9956a2a701942a403da928a63c5e815
Sead Feng <seadfeng@gmail.com>
2024-09-07 18:46:57 +0800

    add footer link

--- src/components/frontend/shared/footer.tsx
+++ src/components/frontend/shared/footer.tsx
@@ -10,6 +10,10 @@ export function Footer() {
           <img alt="Favicon Extractor" src="https://www.faviconextractor.com/favicon/www.faviconextractor.com" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
           <span>Favicon Extractor</span>
         </a>
+        <a href="https://www.favicongenerator.io/" className="text-primary flex items-center">
+          <img alt="Favicon Generator" src="https://www.faviconextractor.com/favicon/www.favicongenerator.io" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
+          <span>Favicon Generator</span>
+        </a>
         <a href="https://www.fontgenerator.dev/" className="text-primary flex items-center">
           <img alt="Font Generator" src="https://www.faviconextractor.com/favicon/www.fontgenerator.dev" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
           <span>Font Generator</span>
